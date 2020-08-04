d7fdb634f9800579c38d0ef989ff9e3f0909bfee
Sven Hessenmüller <sven.hessenmueller@gmail.com>
2020-08-04 20:42:42 +0200

    implements: 'don't open in new tab/window' closes #5

--- templates/bookmarklet/AddToReminiscence.js
+++ templates/bookmarklet/AddToReminiscence.js
@@ -1,7 +1,10 @@
 (function(){
     console.log(`reminiscence-bookmarklet starts`);
+    /*
     window.open(
         `{{target_url}}?directory=AddToReminiscence&url=${encodeURIComponent(document.location.href)}`
     );
+    */
+    window.location.href = `{{target_url}}?directory=AddToReminiscence&url=${encodeURIComponent(document.location.href)}`;
     console.log(`reminiscence-bookmarklet ends`);
 })();
